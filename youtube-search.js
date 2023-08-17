311c4550cd7f6a4ad8d84ef29f66ceadaf044fb7
Pierce A <pbalworth@gmail.com>
2023-08-17 22:09:02 +0000

    Update for more verbose logs youtube-search.js

--- youtube-search.js
+++ youtube-search.js
@@ -9,10 +9,12 @@ const fs = require('fs'); // Import the file system module
 
 const screenshotPath = 'youtube_invaders_video.png';
 const loadTimePath = 'load_time.txt'; // File to save the load time
+const networkRequestsPath = 'network_requests.txt'; // File to save network requests
+const renderStartTimePath = 'render_start_time.txt'; // File to save render start time
 
 const os = require('os');
 
-//get the IP of the eth0 interface so we can tell puppeteer to ignore it
+// Get the IP of the eth0 interface so we can tell puppeteer to ignore it
 function getInterfaceIp(interfaceName) {
     const networkInterfaces = os.networkInterfaces();
     const interfaceDetails = networkInterfaces[interfaceName];
@@ -43,23 +45,40 @@ try {
         args: ['--no-sandbox', '--headless', '--disable-gpu', '--disable-dev-shm-usage', `--netifs-to-ignore=${ipAddress}`]
     });
     const page = await browser.newPage();
+    
+    const navigationStart = await page.evaluate(() => window.performance.timing.navigationStart);
+    
+    // Measure page load time
     await page.goto('https://youtube.com');
+    const loadTime = Date.now() - navigationStart;
+    console.log('Load time:', loadTime, 'ms');
+    fs.writeFileSync(loadTimePath, `Load Time: ${loadTime} ms\n`);
+    
+    // Measure network requests
+    page.on('response', (response) => {
+      const requestUrl = response.url();
+      const statusCode = response.status();
+      const responseTime = response.timing().receiveHeadersEnd - response.timing().sendStart;
+      const requestInfo = `URL: ${requestUrl}, Status: ${statusCode}, Response Time: ${responseTime} ms\n`;
+      fs.appendFileSync(networkRequestsPath, requestInfo);
+    });
+
     await page.type('input#search', 'Iron Maiden Invaders');
     await page.click('button#search-icon-legacy');
     await page.waitForSelector('ytd-thumbnail.ytd-video-renderer');
     await page.waitForTimeout(500);
+
+    // Measure render start time
+    const renderStartTime = await page.evaluate(() => window.performance.timing.domLoading - window.performance.timing.navigationStart);
+    console.log('Render Start Time:', renderStartTime, 'ms');
+    fs.writeFileSync(renderStartTimePath, `Render Start Time: ${renderStartTime} ms\n`);
+
     await page.screenshot({ path: screenshotPath });
     const videos = await page.$$('ytd-thumbnail.ytd-video-renderer');
     await videos[2].click();
     await page.waitForSelector('.html5-video-container');
     await page.waitForTimeout(5000);
     await page.screenshot({ path: screenshotPath });
-
-    // Capture the load time
-    const navigationStart = await page.evaluate(() => window.performance.timing.navigationStart);
-    const loadTime = Date.now() - navigationStart;
-    console.log('Load time:', loadTime, 'ms');
-    fs.writeFileSync(loadTimePath, `${loadTime} ms`);
 
     await browser.close();
     console.log('See screenshot:', screenshotPath);
@@ -69,8 +88,16 @@ try {
 }
 
 
-// const puppeteer = require('puppeteer-core')
-// const screenshot = 'youtube_invaders_video.png'
+
+
+
+
+
+// const puppeteer = require('puppeteer-core');
+// const fs = require('fs'); // Import the file system module
+
+// const screenshotPath = 'youtube_invaders_video.png';
+// const loadTimePath = 'load_time.txt'; // File to save the load time
 
 // const os = require('os');
 
@@ -102,23 +129,31 @@ try {
 //     const browser = await puppeteer.launch({
 //         headless: true,
 //         executablePath: process.env.CHROME_BIN || null,
-//         args: ['--no-sandbox', '--headless', '--disable-gpu', '--disable-dev-shm-usage', '--netifs-to-ignore=${ipAddress}']
-//     })
-//     const page = await browser.newPage()
-//     await page.goto('https://youtube.com')
-//     await page.type('input#search', 'Iron Maiden Invaders')
-//     await page.click('button#search-icon-legacy')
-//     await page.waitForSelector('ytd-thumbnail.ytd-video-renderer')
-//     await page.waitForTimeout(500)
-//     await page.screenshot({ path: 'youtube_invaders_video.png' })
-//     const videos = await page.$$('ytd-thumbnail.ytd-video-renderer')
-//     await videos[2].click()
-//     await page.waitForSelector('.html5-video-container')
-//     await page.waitForTimeout(5000)
-//     await page.screenshot({ path: screenshot })
-//     await browser.close()
-//     console.log('See screenshot: ' + screenshot)
-//   })()
+//         args: ['--no-sandbox', '--headless', '--disable-gpu', '--disable-dev-shm-usage', `--netifs-to-ignore=${ipAddress}`]
+//     });
+//     const page = await browser.newPage();
+//     await page.goto('https://youtube.com');
+//     await page.type('input#search', 'Iron Maiden Invaders');
+//     await page.click('button#search-icon-legacy');
+//     await page.waitForSelector('ytd-thumbnail.ytd-video-renderer');
+//     await page.waitForTimeout(500);
+//     await page.screenshot({ path: screenshotPath });
+//     const videos = await page.$$('ytd-thumbnail.ytd-video-renderer');
+//     await videos[2].click();
+//     await page.waitForSelector('.html5-video-container');
+//     await page.waitForTimeout(5000);
+//     await page.screenshot({ path: screenshotPath });
+
+//     // Capture the load time
+//     const navigationStart = await page.evaluate(() => window.performance.timing.navigationStart);
+//     const loadTime = Date.now() - navigationStart;
+//     console.log('Load time:', loadTime, 'ms');
+//     fs.writeFileSync(loadTimePath, `${loadTime} ms`);
+
+//     await browser.close();
+//     console.log('See screenshot:', screenshotPath);
+//   })();
 // } catch (err) {
-//   console.error(err)
+//   console.error(err);
 // }
+
